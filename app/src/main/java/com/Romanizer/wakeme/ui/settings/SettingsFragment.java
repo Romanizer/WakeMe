01a00e59c98b24393cdafe6062fb1f9a2ec97603
Romanizer <romanhayn99@gmail.com>
2020-07-26 01:54:38 +0200

    ic_settings + removed ViewModels

--- app/src/main/java/com/Romanizer/wakeme/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/Romanizer/wakeme/ui/settings/SettingsFragment.java
@@ -1,25 +1,21 @@
-package com.Romanizer.wakeme.ui.home;
+package com.Romanizer.wakeme.ui.settings;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 
 import com.Romanizer.wakeme.R;
 
-public class HomeFragment extends Fragment {
+public class SettingsFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
+        View root = inflater.inflate(R.layout.fragment_settings, container, false);
 
-        //TODO general info, list of next upcoming alarms,
+        //TODO settings, only settings
 
         return root;
     }
