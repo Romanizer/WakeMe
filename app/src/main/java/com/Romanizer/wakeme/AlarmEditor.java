70a3ec517c9cd68a7668907509197d0dfef3a603
Romanizer <romanhayn99@gmail.com>
2020-07-26 02:05:54 +0200

    added AlarmEditor and AlarmViewer + layouts

---
+++ app/src/main/java/com/Romanizer/wakeme/AlarmEditor.java
@@ -0,0 +1,13 @@
+package com.Romanizer.wakeme;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AlarmEditor extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.alarmeditor);
+    }
+}
---
+++ app/src/main/java/com/Romanizer/wakeme/AlarmViewer.java
@@ -0,0 +1,13 @@
+package com.Romanizer.wakeme;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AlarmViewer extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.alarmviewer);
+    }
+}
