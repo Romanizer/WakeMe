3cd13bb473f59c5a2909cc2c483c801f3ffa11d8
Romanizer <romanhayn99@gmail.com>
2020-07-26 02:00:12 +0200

    refactors of info and alarms

--- app/src/main/java/com/Romanizer/wakeme/ui/alarms/AlarmsFragment.java
+++ app/src/main/java/com/Romanizer/wakeme/ui/alarms/AlarmsFragment.java
@@ -1,23 +1,19 @@
-package com.Romanizer.wakeme.ui.dashboard;
+package com.Romanizer.wakeme.ui.alarms;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 
 import com.Romanizer.wakeme.R;
 
-public class DashboardFragment extends Fragment {
+public class AlarmsFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
+        View root = inflater.inflate(R.layout.fragment_alarms, container, false);
 
         //TODO list of all alarms modifiable,
 
--- app/src/main/java/com/Romanizer/wakeme/ui/info/InfoFragment.java
+++ app/src/main/java/com/Romanizer/wakeme/ui/info/InfoFragment.java
@@ -1,23 +1,19 @@
-package com.Romanizer.wakeme.ui.home;
+package com.Romanizer.wakeme.ui.info;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 
 import com.Romanizer.wakeme.R;
 
-public class HomeFragment extends Fragment {
+public class InfoFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
+        View root = inflater.inflate(R.layout.fragment_info, container, false);
 
         //TODO general info, list of next upcoming alarms,
 
